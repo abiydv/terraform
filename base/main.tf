3574c9dd4410ddb97809c98a7a15c5f26b86e855
Abhinav <32677073+abiydv@users.noreply.github.com>
2019-01-14 10:28:43 +0530

    Create main.tf

---
+++ base/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+    alias                   = "${var.environment}"
+    profile                 = "${var.environment}"
+    shared_credentials_file = "$HOME/.aws/credentials"
+    region                  = "${var.aws_region}"
+}
+
+resource "aws_s3_bucket" "bucket" {
+	provider                 = "aws.${var.environment}"
+	bucket 	                 = "ENTER_YOUR_BUCKET_NAME"
+	versioning {
+		enabled = "true"
+	}
+	lifecycle {
+		prevent_destroy = "true"
+	}
+	server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.key.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "aws_kms_key" "key" {
+	provider = "aws.${var.environment}"
+	description = "This key is used to encrypt bucket objects"
+}
