02284350eb6cffd83fc90c19a3bc1a29c69afcfe
Abhinav <32677073+abiydv@users.noreply.github.com>
2019-01-14 11:06:19 +0530

    Create variables.tf

---
+++ base/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region to use"
+}
+
+variable "aws_account_id" {
+  description = "AWS account ID to use"
+}
+
+variable "environment" {
+  description = "Environment to use (tools/development/qa/production)"
+}
